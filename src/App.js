fa8b46f9eeb4bd8717f26ae7a80cfd89e94ac31e
abr-ya <gamer05@list.ru>
2020-04-18 15:00:13 +0300

    Fix uncovered by linter errors

--- src/App.js
+++ src/App.js
@@ -12,58 +12,62 @@ import '@vkontakte/vkui/dist/vkui.css';
 
 const App = () => {
   const [user, setUser] = useState(null);
-	const [popout, setPopout] = useState(<ScreenSpinner size='large' />);
+  const [popout, setPopout] = useState(<ScreenSpinner size="large" />);
 
-	const buttonSendRequestHandler = () => {
-		console.log('Отправлена заявка:');
-		console.log(user.id, user.first_name, user.last_name)
-	};
+  const buttonSendRequestHandler = () => {
+    console.log('Отправлена заявка:');
+    console.log(user.id, user.first_name, user.last_name);
+  };
 
-	useEffect(() => {
-		async function fetchData() {
-			try {
-				const user = await bridge.send('VKWebAppGetUserInfo', {});
-				setUser(user);
-			} catch (err) {
-				console.log(err);
-			}
-			setPopout(null);
-		}
-		fetchData();
-	}, []);
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const userFromVK = await bridge.send('VKWebAppGetUserInfo', {});
+        setUser(userFromVK);
+      } catch (err) {
+        // eslint-disable-next-line no-console
+        console.log(err);
+      }
+      setPopout(null);
+    }
+    fetchData();
+  }, []);
+
   return (
-		<View activePanel="main" popout={popout}>
-			<Panel id="main">
-				<PanelHeader>Welcome</PanelHeader>
-				{user &&
-					<Div>
-						<Group title="User Data Fetched with VK Bridge">
-							<Cell
-								before={user.photo_200 ? <Avatar src={user.photo_200}/> : null}
-								description={'нажмите кнопку ниже чтобы отправить заявку:'}
-							>
-								{`${user.first_name} ${user.last_name}`}
-							</Cell>
-						</Group>
-						<Button
-							onClick={buttonSendRequestHandler}
-							size="xl"
-							mode="secondary"
-						>
-							Отправить заявку
-						</Button>
-					</Div>
-				}
-				<Epic tabbar={
-					<Tabbar>
-						<TabbarItem text="Профиль"><Icon24UserOutline/></TabbarItem>
-						<TabbarItem text="Чат"><Icon24ChatsOutline/></TabbarItem>
-						<TabbarItem text="Магазин"><Icon24Gift/></TabbarItem>
-					</Tabbar>
-				}/>
-			</Panel>
-		</View>
+    <View activePanel="main" popout={popout}>
+      <Panel id="main">
+        <PanelHeader>Welcome</PanelHeader>
+        {user
+        && (
+          <Div>
+            <Group title="User Data Fetched with VK Bridge">
+              <Cell
+                before={user.photo_200 ? <Avatar src={user.photo_200} /> : null}
+                description="нажмите кнопку ниже чтобы отправить заявку:"
+              >
+                {`${user.first_name} ${user.last_name}`}
+              </Cell>
+            </Group>
+            <Button
+              onClick={buttonSendRequestHandler}
+              size="xl"
+              mode="secondary"
+            >
+              Отправить заявку
+            </Button>
+          </Div>
+        )}
+        <Epic tabbar={(
+          <Tabbar>
+            <TabbarItem text="Профиль"><Icon24UserOutline /></TabbarItem>
+            <TabbarItem text="Чат"><Icon24ChatsOutline /></TabbarItem>
+            <TabbarItem text="Магазин"><Icon24Gift /></TabbarItem>
+          </Tabbar>
+        )}
+        />
+      </Panel>
+    </View>
   );
-}
+};
 
 export default App;
