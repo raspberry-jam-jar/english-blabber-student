337781072d82e44b689508db9d3b8d3cbc3fbde0
Yaroslav <gamer05@list.ru>
2020-05-23 11:41:40 +0300

    Add switch of panels

--- src/App.js
+++ src/App.js
@@ -13,10 +13,23 @@ import '@vkontakte/vkui/dist/vkui.css';
 const App = () => {
   const [user, setUser] = useState(null);
   const [popout, setPopout] = useState(<ScreenSpinner size="large" />);
+  const [activePanel, setActivePanel] = useState('main');
 
   const buttonSendRequestHandler = () => {
     console.log('Отправлена заявка:');
     console.log(user.id, user.first_name, user.last_name);
+  };
+
+  const profileIconClickHandler = () => {
+    setActivePanel('main');
+  };
+
+  const chatIconClickHandler = () => {
+    setActivePanel('chat');
+  };
+
+  const storeIconClickHandler = () => {
+    setActivePanel('store');
   };
 
   useEffect(() => {
@@ -33,40 +46,49 @@ const App = () => {
     fetchData();
   }, []);
 
+  const customTabbar = () => (
+    <Tabbar>
+      <TabbarItem text="Профиль" onClick={profileIconClickHandler}><Icon24UserOutline /></TabbarItem>
+      <TabbarItem text="Чат" onClick={chatIconClickHandler}><Icon24ChatsOutline /></TabbarItem>
+      <TabbarItem text="Магазин" onClick={storeIconClickHandler}><Icon24Gift /></TabbarItem>
+    </Tabbar>
+  );
+
   return (
-    <View activePanel="main" popout={popout}>
-      <Panel id="main">
-        <PanelHeader>Welcome</PanelHeader>
-        {user
-        && (
-          <Div>
-            <Group title="User Data Fetched with VK Bridge">
-              <Cell
-                before={user.photo_200 ? <Avatar src={user.photo_200} /> : null}
-                description="нажмите кнопку ниже чтобы отправить заявку:"
+    <>
+      <View activePanel={activePanel} popout={popout}>
+        <Panel id="main">
+          <PanelHeader>Профиль</PanelHeader>
+          {user
+          && (
+            <Div>
+              <Group title="User Data Fetched with VK Bridge">
+                <Cell
+                  before={user.photo_200 ? <Avatar src={user.photo_200} /> : null}
+                  description="нажмите кнопку ниже чтобы отправить заявку:"
+                >
+                  {`${user.first_name} ${user.last_name}`}
+                </Cell>
+              </Group>
+              <Button
+                onClick={buttonSendRequestHandler}
+                size="xl"
+                mode="secondary"
               >
-                {`${user.first_name} ${user.last_name}`}
-              </Cell>
-            </Group>
-            <Button
-              onClick={buttonSendRequestHandler}
-              size="xl"
-              mode="secondary"
-            >
-              Отправить заявку
-            </Button>
-          </Div>
-        )}
-        <Epic tabbar={(
-          <Tabbar>
-            <TabbarItem text="Профиль"><Icon24UserOutline /></TabbarItem>
-            <TabbarItem text="Чат"><Icon24ChatsOutline /></TabbarItem>
-            <TabbarItem text="Магазин"><Icon24Gift /></TabbarItem>
-          </Tabbar>
-        )}
-        />
-      </Panel>
-    </View>
+                Отправить заявку
+              </Button>
+            </Div>
+          )}
+        </Panel>
+        <Panel id="chat">
+          <PanelHeader>Чат</PanelHeader>
+        </Panel>
+        <Panel id="store">
+          <PanelHeader>Магазин</PanelHeader>
+        </Panel>
+      </View>
+      <Epic tabbar={customTabbar()} />
+    </>
   );
 };
 
