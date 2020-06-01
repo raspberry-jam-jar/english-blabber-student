7c72b08a8414f4f5ef09ada2115a1ae6fced4af0
Yaroslav <gamer05@list.ru>
2020-06-01 07:47:50 +0300

    Add auth method

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import 'core-js/features/map';
 import 'core-js/features/set';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import bridge from '@vkontakte/vk-bridge';
 import App from './App';
 
