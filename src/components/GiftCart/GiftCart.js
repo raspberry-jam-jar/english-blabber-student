69224036f5077685b5cadbb95392271bd1275915
Полина Попова <popova_pv@km-union.ru>
2020-07-12 11:36:59 +0300

    Add buy gift alert for store panel

--- src/components/GiftCart/GiftCart.js
+++ src/components/GiftCart/GiftCart.js
@@ -1,28 +1,81 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Button, Alert } from '@vkontakte/vkui';
 
 import styles from './giftCart.module.scss';
 import defaultImage from './giftbox.png';
 
-const GiftCart = ({ gift, isBackpack }) => {
+const GiftCart = ({ gift, isBackpack, setPopout }) => {
   const imageUrl = gift.image ? gift.image : defaultImage;
   const remainLookup = gift.remain != null ? `${gift.remain} шт.` : '';
   const boughtQuantityLookup = `${gift.quantity} шт.`;
+  const buttonName = isBackpack ? 'Использовать' : 'Купить';
+  const alertBody = isBackpack ? `Использовать "${gift.name}"?` : `Купить "${gift.name}" за ${gift.price} профкоинов?`;
+
+  const closePopout = () => {
+    setPopout(null);
+  };
+
+  const confirmActionPopout = (
+    <Alert
+      actions={[{
+        title: 'Назад',
+        autoclose: true,
+        mode: 'cancel',
+      }, {
+        title: buttonName,
+        autoclose: true,
+      }]}
+      onClose={closePopout}
+    >
+      <h2>Подтвердите действие</h2>
+      <p>
+        {alertBody}
+      </p>
+    </Alert>
+  );
+  const declineActionPopout = (
+    <Alert
+      actions={[{
+        title: 'Назад',
+        autoclose: true,
+        mode: 'cancel',
+      }]}
+      onClose={closePopout}
+    >
+      <h2>Действие нельзя выполнить</h2>
+      <p>
+        У
+        {gift.isGroupWide ? ' когото-то из группы ' : ' вас '}
+        недостаточно профкоинов для покупки
+      </p>
+    </Alert>
+  );
 
   return (
-    <a className={`${styles.giftContainer} ${gift.canBuy ? '' : styles.isTooExpensive}`} href="some-href">
+    <div
+      className={`${styles.giftContainer} ${gift.canBuy ? '' : styles.isTooExpensive}`}
+    >
       <div className={styles.giftContainerImg}>
         <img src={imageUrl} alt="gift box" />
       </div>
-      <div className={styles.giftInfoPanel}>
-        <div>
-          <span className={styles.coin} />
-          {gift.price}
+      <div>
+        <div className={styles.giftInfoPanel}>
+          <div>
+            <span className={styles.coin} />
+            {gift.price}
+          </div>
+          <div>{isBackpack ? boughtQuantityLookup : remainLookup}</div>
         </div>
-        <div>{isBackpack ? boughtQuantityLookup : remainLookup}</div>
+        <div>{gift.name}</div>
       </div>
-      <div>{gift.name}</div>
-    </a>
+      <Button
+        style={{ marginTop: 6 }}
+        onClick={() => setPopout(gift.canBuy ? confirmActionPopout : declineActionPopout)}
+      >
+        {buttonName}
+      </Button>
+    </div>
   );
 };
 
@@ -35,8 +88,10 @@ GiftCart.propTypes = {
     remain: PropTypes.number,
     image: PropTypes.string,
     quantity: PropTypes.number,
+    isGroupWide: PropTypes.bool,
   }).isRequired,
   isBackpack: PropTypes.bool,
+  setPopout: PropTypes.func.isRequired,
 };
 
 GiftCart.defaultProps = {
