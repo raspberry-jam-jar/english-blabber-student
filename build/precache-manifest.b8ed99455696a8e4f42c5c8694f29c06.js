e3e618540fa3385333526be9cbbb34839f704132
abr-ya <gamer05@list.ru>
2020-08-23 17:06:15 +0300

    Fix favicon and add build

---
+++ build/precache-manifest.b8ed99455696a8e4f42c5c8694f29c06.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1a20952a2ef8e778d8f94771168a85f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0779124314cf15839ffb",
+    "url": "/static/css/2.1e562f28.chunk.css"
+  },
+  {
+    "revision": "4e04f81c58f9da841e14",
+    "url": "/static/css/main.1db7760a.chunk.css"
+  },
+  {
+    "revision": "0779124314cf15839ffb",
+    "url": "/static/js/2.dd476c6c.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/static/js/2.dd476c6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e04f81c58f9da841e14",
+    "url": "/static/js/main.1382990e.chunk.js"
+  },
+  {
+    "revision": "1a91dc7b691e5ddc279d",
+    "url": "/static/js/runtime-main.d162909d.js"
+  }
+]);
